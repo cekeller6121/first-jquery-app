889c45b7ce2d56db67716df402b2a4e834157943
chadwick <cekeller6121@yahoo.com>
2017-09-07 13:39:18 -0400

    add toggle scripts

---
+++ tgglscripts.js
@@ -0,0 +1,21 @@
+console.log("tgglscripts.js file connected");
+
+$(document).ready(function() {
+
+  $('#btn1').on('click', function() {
+    $('#item1').toggle();
+  });
+
+  $('#btn2').on('click', function() {
+    $('#item2').toggle();
+  });
+
+  $('#btn3').on('click', function() {
+    $('#item3').toggle();
+  });
+
+  $('#btn4').on('click', function() {
+    $('#item4').toggle();
+  });
+
+});
